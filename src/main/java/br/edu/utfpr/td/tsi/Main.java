8f83e9dd872c974d32d3ad40d866340cc54053c9
Alex Sander Leite de Almeida <66784006+KalexSan@users.noreply.github.com>
2024-09-02 21:57:06 -0300

    Descobrir o erro
    Desgraça do controller kkk

--- src/main/java/br/edu/utfpr/td/tsi/Main.java
+++ src/main/java/br/edu/utfpr/td/tsi/Main.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({ "file:./applicationContext.xml" })
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
          System.setProperty("server.servlet.context-path", "/posto-saude");
 		SpringApplication.run(Main.class, args);
     }
--- src/main/java/br/edu/utfpr/td/tsi/controle/PacienteController.java
+++ src/main/java/br/edu/utfpr/td/tsi/controle/PacienteController.java
@@ -3,6 +3,7 @@ package br.edu.utfpr.td.tsi.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import br.edu.utfpr.td.tsi.DAO.PacienteDAO;
 import br.edu.utfpr.td.tsi.MODELO.Endereco;
 import br.edu.utfpr.td.tsi.MODELO.Paciente;
 
+@Controller
 public class PacienteController {
     
     @Autowired
@@ -26,9 +28,9 @@ public class PacienteController {
 
     }
 
-    @GetMapping(value = "/cadastrarPaciente")
+    @GetMapping(value = "/cadastrarpaciente")
     public String cadastrarPaciente() {
-        return "cadastrarPaciente";
+        return "cadastrarpaciente";
     }
 
     @PostMapping("/cadastrarPaciente")
